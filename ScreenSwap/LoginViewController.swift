b3471f279c8d989c12de562caed8256490ebacfb
Simon Zhen <me@PeeSee.local>
2017-04-04 23:08:44 -0700

    first commit

---
+++ ScreenSwap/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScreenSwap
+//
+//  Created by Simon Zhen on 4/4/17.
+//  Copyright © 2017 Simon Zhen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
